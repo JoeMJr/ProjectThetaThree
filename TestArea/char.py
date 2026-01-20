8978a0d0480c4097eab78fed02d6a2f1aef9b380
JoeMJr <josephmiddletonjr@outlook.com>
2026-01-19 21:45:20 -0500

    set up basic stats

--- TestArea/char.py
+++ TestArea/char.py
@@ -7,7 +7,8 @@ class Character:
         self.current_loc = current_loc
 
 class Player:
-    pass
+    def __init__(self):
+        pass
 
 class AreaBoss:
     pass
@@ -20,4 +21,37 @@ class FinalBoss:
 
 class DialogNPC:
     pass
+
+class CharStats:
+    def __init__(self, hp, phy, spc, wit, spd):
+        self.__hp = hp
+        self.__phy = phy
+        self.__spc = spc
+        self.__wit = wit
+        self.__spd = spd
+    # GET METHODS
+    def getHp(self):
+        return self.__hp
+    def getPhy(self):
+        return self.__phy
+    def getSpc(self):
+        return self.__spc
+    def getWit(self):
+        return self.__wit
+    def getSpd(self):
+        return self.__spd
+    def getStatLine(self):
+        return "Health: " + self.__hp + ", Physical: " + self.__phy + ", Special: " + self.__spc + ", Wit: " + self.__wit + ", Speed: " + self.__spd
+    
+    # SET METHODS
+    def setHp(self, hp):
+        self.__hp = self.__hp + hp
+    def setPhy(self, phy):
+        self.__phy = self.__phy + phy
+    def setSpc(self, spc):
+        self.__spc = self.__spc + spc
+    def setWit(self, wit):
+        self.__wit = self.__wit + wit
+    def setSpd(self, spd):
+        self.__spd = self.__spd + spd
 
--- TestArea/loc.py
+++ TestArea/loc.py
@@ -1,2 +1,12 @@
 # Directed Graph of locations the player will go through
 
+loc_dict = {
+    '0': "Test"
+}
+
+def get_loc_desc(loc_id):
+    pass
+
+def get_loc_info():
+    pass
+
