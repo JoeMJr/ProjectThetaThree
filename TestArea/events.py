d67de44d25ce3725e898c7c063080fcffca24776
JoeMJr <josephmiddletonjr@outlook.com>
2026-03-20 20:07:14 -0400

    event stuff and loc thing

--- TestArea/events.py
+++ TestArea/events.py
@@ -2,6 +2,17 @@
 # Will affect Characters (Player and maybe more)
 import char as Char
 
+# List of Events for each location
+
+class Event:
+    def __init__(self, name):
+        self.name = name
+        # Event Type - What the event does (i might make this an int)
+        # One for moving locations, two stats stuff, three
+        # Array of stats effected
+
+
+'''
 def game_init(passed_char):
     pass
 
@@ -9,3 +20,4 @@ def game_loop_func():
     game_layer = 0
     print("")
 
+'''
--- TestArea/loc.py
+++ TestArea/loc.py
@@ -1,7 +1,9 @@
 # Directed Graph of locations the player will go through
 
 loc_dict = {
-    '0': "Test"
+    '0': "Test",
+    '1': "Start"
+    
 }
 
 def get_loc_desc(loc_id):
